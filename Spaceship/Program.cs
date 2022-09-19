94853ef6f5a34a50d466290b9f79cce06f974ac2
Mikhail <smirnow.mishas2017@gmail.com>
2022-09-19 15:59:26 +0600

    thcom

--- Spaceship/Program.cs
+++ Spaceship/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("SIUUUUUUUUUUUUU");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("SIUUUUUU");
+           Console.WriteLine("Ronaldo");
+        }
+    }
+}
