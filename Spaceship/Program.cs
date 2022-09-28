9c35e519c31f58adfd9e85bf7743b7f87981dbc3
Mikhail <smirnow.mishas2017@gmail.com>
2022-09-28 18:04:21 +0600

    forfun

--- Spaceship/Program.cs
+++ Spaceship/Program.cs
@@ -4,14 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lessons
+public class Space
 {
-    class Program
+    public double x, y;
+    public double XX
     {
-        static void Main(string[] args)
+        get
         {
-           Console.WriteLine("SIUUUUUU");
-           Console.WriteLine("Ronaldo");
+            Console.Write("Введите x: ");
+            return x = Convert.ToDouble(Console.ReadLine());
         }
     }
+    public double YY
+    {
+        get
+        {
+            Console.Write("Введите y: ");
+            return y = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+    static public void Print(double spaceship, double spaceship1)
+    {
+        Console.WriteLine($"Pos: {spaceship} {spaceship1}");
+    }
+}
+
+class Spaceship : Space
+{
+    static void Main(string[] args)
+        {
+            Space spaceship = new Space();
+            Print(spaceship.XX, spaceship.YY);
+        }
 }
