e00640b129c7cf4c59e94dbbb0b7534de4265f92
Mikhail <smirnow.mishas2017@gmail.com>
2022-10-14 23:40:45 +0600

    first_wrong_test

--- SpaceBattle.Lib.Test/UnitTest1.cs
+++ SpaceBattle.Lib.Test/UnitTest1.cs
@@ -1,11 +1,20 @@
+using Moq;
 namespace SpaceBattle.Lib.Test;
-
-public class UnitTest1
+public class MoveCommandTest
 {
     [Fact]
     public void TestPositiveMove()
     {
+        // Mock<IMovable> movableMock = new Mock<IMovable>();
+        // movable.SetupProperty(i => i.Position, new Vector(12,5));
+        // movable.SetupGet<Vector>(i => i.Velocity).Returns(new Vector(-7,3));
+        // ICommand c = new MoveCommand(movableMock.Object);
+
         Mock<IMovable> movableMock = new Mock<IMovable>();
-        ICommand c = new MoveCommand(movableMock.Object);
+        movableMock.SetupProperty(i => i.Position, new Vector(12, 5));
+        movableMock.SetupGet<Vector>(i => i.Velocity).Returns(new Vector(-7, 3));
+        ICommand movecommand = new MoveCommand(movableMock.Object);
+        movecommand.execute();
+        Assert.Equal(new Vector(5, 8), movableMock.Object.Position);
     }
 }
