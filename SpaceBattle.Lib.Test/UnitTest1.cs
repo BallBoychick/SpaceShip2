55535c5e906d5d1418b5d673756984986732a92d
Mikhail <smirnow.mishas2017@gmail.com>
2022-10-16 16:59:49 +0600

    move_command_tests_done

--- SpaceBattle.Lib.Test/UnitTest1.cs
+++ SpaceBattle.Lib.Test/UnitTest1.cs
@@ -30,8 +30,27 @@ public class MoveCommandTest
     }
 
     [Fact]
-    public void MoveCommandTestCantGetVelicity()
+    public void MoveCommandTestCantGetInstantaneousVelocity()
     {
+        // Arrange
+        Mock<IMovable> movableMock = new Mock<IMovable>();
+        movableMock.SetupProperty(i => i.Position, new Vector(12, 5));
+        movableMock.SetupGet<Vector>(i => i.Velocity).Throws<Exception>();
+        ICommand movecommand = new MoveCommand(movableMock.Object);
+        // Assert
+        Assert.Throws<Exception>(() => movecommand.execute());
+    }
 
+    [Fact]
+    public void MoveCommandTesCantSetPosition()
+    {
+        // Arrange
+        Mock<IMovable> movableMock = new Mock<IMovable>();
+        movableMock.SetupProperty(i => i.Position, new Vector(12, 5));
+        movableMock.SetupSet<Vector>(i => i.Position = It.IsAny<Vector>()).Throws<ArgumentException>();
+        movableMock.SetupGet<Vector>(i => i.Velocity).Returns(new Vector(-7,3));
+        ICommand movecommand = new MoveCommand(movableMock.Object);
+        // Assert
+        Assert.Throws<ArgumentException>(() => movecommand.execute());
     }
 }
