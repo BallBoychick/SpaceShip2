b1b6751073514f6ed8576b475609db208597dbcb
Mikhail <smirnow.mishas2017@gmail.com>
2022-10-16 16:10:53 +0600

    2_right_tests

--- SpaceBattle.Lib.Test/UnitTest1.cs
+++ SpaceBattle.Lib.Test/UnitTest1.cs
@@ -1,20 +1,37 @@
-using Moq;
 namespace SpaceBattle.Lib.Test;
 public class MoveCommandTest
 {
     [Fact]
-    public void TestPositiveMove()
+    public void MoveCommandTestPositive()
     {
-        // Mock<IMovable> movableMock = new Mock<IMovable>();
-        // movable.SetupProperty(i => i.Position, new Vector(12,5));
-        // movable.SetupGet<Vector>(i => i.Velocity).Returns(new Vector(-7,3));
-        // ICommand c = new MoveCommand(movableMock.Object);
-
+        // Arrange
         Mock<IMovable> movableMock = new Mock<IMovable>();
         movableMock.SetupProperty(i => i.Position, new Vector(12, 5));
-        movableMock.SetupGet<Vector>(i => i.Velocity).Returns(new Vector(-7, 3));
+        movableMock.SetupGet(i => i.Velocity).Returns(new Vector(-7, 3));
         ICommand movecommand = new MoveCommand(movableMock.Object);
+        // Act
         movecommand.execute();
+        // Assert
         Assert.Equal(new Vector(5, 8), movableMock.Object.Position);
+    }
+
+    [Fact]
+    public void MoveCommandTestCantGetPosition()
+    {
+        // Arrange
+        Mock<IMovable> movableMock = new Mock<IMovable>();
+        movableMock.SetupProperty(i => i.Position, new Vector(12, 5));
+        movableMock.SetupGet<Vector>(i => i.Velocity).Throws<ArgumentException>();
+        ICommand movecommand = new MoveCommand(movableMock.Object);
+        // Act
+
+        //Assert
+        Assert.Throws<ArgumentException>(() => movecommand.execute());
+    }
+
+    [Fact]
+    public void MoveCommandTestCantGetVelicity()
+    {
+
     }
 }
